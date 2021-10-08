646ff82e1f3eca54b489ee8eff092e6b8c9d778e
Ali Gasimzade <38853700+quarterpound@users.noreply.github.com>
2021-10-08 14:23:36 +0400

    Removed not needed pics

--- src/components/ImageSetChanger.js
+++ src/components/ImageSetChanger.js
@@ -3,7 +3,6 @@ import {useEffect, useState} from "react";
 import img0 from "../assets/pic/8.webp";
 import img1 from "../assets/pic/90.webp";
 import img2 from "../assets/pic/99.webp";
-import img3 from "../assets/pic/13.webp";
 import img4 from "../assets/pic/14.webp";
 import img5 from "../assets/pic/15.webp";
 import img6 from "../assets/pic/20.webp";
@@ -13,10 +12,10 @@ export default function ImageSetChanger() {
     const [currentSet, setCurrentSet] = useState(0);
 
 
-    const set0 = [img0, img4, img2, img1];
-    const set1 = [img1, img2, img0, img4];
-    const set2 = [img4, img1, img0, img6];
-    const set3 = [img6, img5, img0, img4];
+    const set0 = [img6, img4, img2, img1];
+    const set1 = [img1, img2, img6, img4];
+    const set2 = [img4, img1, img2, img6];
+    const set3 = [img6, img5, img2, img4];
 
     const sets = [set0, set1, set2, set3];
 
